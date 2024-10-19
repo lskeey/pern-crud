47f3a84b214c61c1b9b0c27977278fc7492f733b
Muhammad Alviansyah <muhammd.alviansyah@gmail.com>
2024-10-19 21:26:44 +0700

    feat(api): add basic GET route for root endpoint

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ app.use(express.json)
 
 const port = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
     
